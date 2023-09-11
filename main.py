cde842029d244a04295c3ae6ae26e102b7236c77
A Chethan Reddy <69640722+chethanreddy123@users.noreply.github.com>
2023-09-11 11:34:43 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,127 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+import requests
+import logging
+
+
+app = FastAPI()
+
+properties = {
+    'base_url': "https://na-1-dev.api.opentext.com",
+    'css_url': "https://css.na-1-dev.api.opentext.com",
+    'tenant_id': "86581e21-636f-4e1d-8336-061ddcd9293a",
+    'username': "aioverflow.ml@gmail.com",
+    'password': "!$hQPPh7HJnpC.7",
+    'client_id': "eph2Is82hQZ6ltgrP4NjLgBuM96261Fv",
+    'client_secret': "0p5Pz6MaHEThN1MV"
+}
+
+def get_auth_token():
+    print("...Requesting New Authentication Token")
+
+    url = f"{properties['base_url']}/tenants/{properties['tenant_id']}/oauth2/token"
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    payload = {
+        'client_id': properties['client_id'],
+        'client_secret': properties['client_secret'],
+        'grant_type': "client_credentials",
+        'username': properties['username'],
+        'password': properties['password']
+    }
+
+    try:
+        response = requests.post(url, headers=headers, json=payload)
+
+        if not response.ok:
+            print("Error acquiring authentication token")
+            print("Authentication Failed. Please verify your credentials in properties.py")
+            return
+
+        data = response.json()
+        return data['access_token']
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def handle_upload_to_risk_guard(accessToken, file):
+    piiData = ""
+    piiDataPlaceholder = ""
+    tmeResults = ""
+
+    if not accessToken:
+        print("Missing Authentication Token")
+        return
+
+    print("...Processing Text Mining")
+
+    formData = {'File': ('sample.pdf', open('sample.pdf', 'rb'), 'application/pdf')}
+
+    headers = { 
+        'Authorization': f'Bearer {accessToken}', 
+        'Accept': 'application/json'
+    }
+
+    response = requests.post(
+        f'{properties["base_url"]}/mtm-riskguard/api/v1/process',
+        headers=headers,
+        files=formData
+    )
+
+    if response.status_code == 200:
+        data = response.json()
+        if not data.get('results', {}).get('tme', {}).get('result'):
+            print("...No searchable PII data found")
+            return
+        print(f'...{data["header"]["status"]["description"]}')
+        tmeResults = data
+        return tmeResults
+    elif response.status_code == 401:
+        print("...Authentication Token has expired. Please obtain a new token.")
+    else:
+        print(f'...Error: {response.text}')
+            
+def display_pii_data(tme_results):
+    pii_data = ""
+    pii_data_placeholder = ""
+
+    if len(tme_results) == 0:
+        pii_data_placeholder = "...No PII data to display"
+    else:
+        for extracted_term in tme_results:
+            cartridge_id = extracted_term['CartridgeID']
+            subterm_value = extracted_term['Subterms']['Subterm'][0]['value']
+            pii_data += f'{cartridge_id} = {subterm_value}\n'
+
+    return pii_data
+
+
+def save_pdf_file(file, filename):
+    # Save the uploaded file as 'sample.pdf'
+    with open(filename, "wb") as pdf_file:
+        pdf_file.write(file.read())
+
+@app.post("/process_upload/")
+async def process_upload(file: UploadFile = File(...)):
+    access_token = get_auth_token()
+    if not access_token:
+        return JSONResponse(content={"error": "Error acquiring authentication token"}, status_code=500)
+
+    # Get the file name
+    file_name = file.filename
+
+    # If the file is not in PDF format, save it as sample.pdf
+    if not file_name.endswith('.pdf'):
+        with open('sample.pdf', 'wb') as f:
+            f.write(await file.read())
+        file_name = 'sample.pdf'
+
+    logging.info(f"Processing file: {file_name}")
+    logging.info("access_token: " + access_token)
+
+    tme_results = handle_upload_to_risk_guard(access_token, file_name)
+ 
+
+    return tme_results
+
